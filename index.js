b96c2f038c4fe1e05bdc2bad799e016847d7231e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-02 20:21:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function ReturnSuccess(){
-    return res.json({ Status: "Success"})
+function ReturnSuccess(res){
+    return res.status(200).json({ Status: "Success"})
 }
 
+function ReturnSuccessMsg(res, msg){
+    return res.status(200).json({ Status: "Success", Message: msg})
+}
+
+module.exports = { ReturnSuccess, ReturnSuccessMsg };
